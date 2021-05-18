6e426e03256c8569518284dd6ffacd0e4d0fc237
nadila_azevedo <ndsda.eng19@uea.edu.br>
2021-05-18 16:19:44 -0400

    primeira versao

---
+++ src/models/cliente.ts
@@ -0,0 +1,16 @@
+class Cliente {
+    pagamento : Number;
+    dia : String;
+    filme : String;
+
+     constructor(pagamento: number, dia: string, filme: string){
+         this.pagamento = pagamento;
+         this.dia = dia
+         this.filme = filme
+     }
+     escolha_do_filme(){
+         
+     }
+
+
+}
